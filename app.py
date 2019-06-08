4c90674faafa468a196bb5f13ce9507dfda0aefc
Sidharth Raguraman <ragurama@usc.edu>
2019-06-07 17:32:44 -0700

    before debug

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask
+import requests
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/search/<search_term>')
+def query_giphy(search_term):
+    url = 'http://api.giphy.com/v1/gifs/search'
+    key = 'vIa6iVvRwwONNxkoXfSvGnyKuGvDgg3d'
+    num_requests_allowed = 5
+    payload = {'q':search_term, 'api_key': key, 'limit': num_requests_allowed}
+    final_response = parse_json_response(requests.get(url, params=payload))
+    return json.dumps({'data': final_response})
+
+
+def parse_json_response(giphy_request):
+    response = giphy_request.json()['data']
+    if len(response) > 4:
+        return [{'gif_id': response['id'], 'url': response['url']}]
+    else:
+        return []
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080, debug=True)
